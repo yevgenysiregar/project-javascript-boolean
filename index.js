05039d2e886a90aeaf84fcdcb45c3931e6e95419
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-04-04 17:40:59 +0700

    add index.js and add text inside

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("true");
+console.log("false");
+console.log("true, false");
+
+const toggle = true;
+
+console.log(`Earth is globe is ${toggle}`);
+
+alert(`Earth is globe is ${toggle}`);
